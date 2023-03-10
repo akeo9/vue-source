0ea5847e4b2874ca54961ec96ab93437fd9405ae
Alicia Keo <akeo@eagles.bridgewater.edu>
2023-03-09 21:18:13 -0500

    app10

---
+++ app10/src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+  import { ref, reactive } from 'vue'
+  import Form from './components/Form.vue';
+
+  let cards = reactive([])
+
+  const name = ref('')
+  const animal = ref('')
+
+  function addCard() {
+    cards.push({
+      name: name.value,
+      animal: animal.value
+    })
+  }
+</script>
+
+<template>
+  <div>
+    <section>
+      <div id="form">
+          <Form 
+            title="Name"
+            v-model:name="name"
+            v-model:animal="animal"
+            @addCard="addCard" />
+      </div>
+    </section>
+
+    <section id="card">
+      <div v-for="card in cards">
+          <span>{{ card }}</span><br>
+      </div>
+    </section>
+    </div>
+</template>
+
+<style>
+  main {
+  display: flex;
+  margin: 10px 10%;
+  }
+  section {
+    margin: 10px;
+  }
+  #form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  #card {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    border: solid 1px black;
+  }
+  #card .card {
+    max-width: 500px;
+    margin: 0 auto;
+    padding-bottom: 15px;
+  }
+  button {
+    margin: 3px 0 0 0;
+  }
+</style>
---
+++ app10/src/components/Form.vue
@@ -0,0 +1,47 @@
+<script setup>
+    import {computed} from 'vue'
+
+    const props = defineProps(['name', 'animal'])
+    const emit = defineEmits(['update:name', 'update:animal', 'addCard'])
+
+    const value1 = computed({
+            get() {return props.name},
+            set(arg) {emit('update:name', arg)}
+    })
+
+    const value2 = computed({
+            get() {return props.animal},
+            set(arg) {emit('update:animal', arg)}
+    })
+
+</script>
+
+<template>
+    <div id=form>
+        <form>
+            <div id="card">
+                <label for="name">Name</label>
+                <input id="name" v-model="value1"/>
+            
+                <label for="animal">Favorite Animal</label>
+                <input id="animal" v-model="value2"/>
+                
+                <button type="button" @click="$emit('addCard')">Add Card</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<style>
+#card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 20px;
+    border: solid 1px lightgray;
+
+    margin: 0 30px;
+}
+</style>
